43e1222ba53eee38e29a21c2a555b4b6a7f1d772
Md Imam Hossain <imamagun94@gmail.com>
2023-11-09 23:01:15 +0600

    feat: Main activity design implemented

--- app/src/main/java/com/newagedevs/musicoverlay/MainActivity.kt
+++ app/src/main/java/com/newagedevs/musicoverlay/MainActivity.kt
@@ -2,41 +2,28 @@ package com.newagedevs.musicoverlay
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import dev.oneuiproject.oneui.layout.ToolbarLayout
+import com.newagedevs.musicoverlay.databinding.ActivityMainBinding
 import dev.oneuiproject.oneui.utils.ActivityUtils
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val toolbarLayout = findViewById<ToolbarLayout>(R.id.toolbar_layout)
-        toolbarLayout.setNavigationButtonAsBack()
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
 
-        /*TODO:
-         *  - change packageName and name
-         *  - check other TODOs
-         * */
-    }
+        binding.toolbarLayout.setNavigationButtonAsBack()
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_app_info) {
-            ActivityUtils.startPopOverActivity(
-                this,
-                Intent(this, AboutActivity::class.java),
-                null,
-                ActivityUtils.POP_OVER_POSITION_RIGHT or ActivityUtils.POP_OVER_POSITION_TOP
-            )
-            return true
+        binding.aboutMusicOverlay.setOnClickListener {
+            startActivity(Intent(this, AboutActivity::class.java))
         }
-        return false
+
     }
+
+
 }
