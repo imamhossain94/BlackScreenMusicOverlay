8ee4664650f2a46c315715e334643ebbc25d19dd
imammasleap <imam@masleap.io>
2023-12-11 18:41:04 +0600

    Permission check added

--- app/src/main/java/com/newagedevs/musicoverlay/activities/PermissionActivity.kt
+++ app/src/main/java/com/newagedevs/musicoverlay/activities/PermissionActivity.kt
@@ -1,33 +1,40 @@
 package com.newagedevs.musicoverlay.activities
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.fondesa.kpermissions.allGranted
-import com.fondesa.kpermissions.anyGranted
-import com.fondesa.kpermissions.extension.permissionsBuilder
-import com.fondesa.kpermissions.extension.send
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.newagedevs.musicoverlay.databinding.ActivityPermissionBinding
+import dev.oneuiproject.oneui.widget.Toast
+
 
 class PermissionActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPermissionBinding
-    private val audioPermissions = permissionsBuilder(Manifest.permission.RECORD_AUDIO).build()
-    //        private val overlayPermissions = permissionsBuilder(Manifest.permission.SYSTEM_ALERT_WINDOW).build()
-    private val allPermissions = permissionsBuilder(Manifest.permission.RECORD_AUDIO).build()
+    private lateinit var overlayPermissionLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityPermissionBinding.inflate(layoutInflater);
+        binding = ActivityPermissionBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if(isAllPermissionsAreGranted()) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
 
         binding.permissionRecordAudioCard.isEnabled = true
         binding.permissionAppearOnTopCard.isEnabled = true
-        binding.startMainActivity.isEnabled = false
+        binding.startMainActivity.isEnabled = isAllPermissionsAreGranted()
 
         binding.startMainActivity.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
@@ -35,27 +42,58 @@ class PermissionActivity : AppCompatActivity() {
         }
 
         binding.permissionRecordAudioCard.setOnClickListener {
-            audioPermissions.send {
-                binding.startMainActivity.isEnabled = it.anyGranted()
-            }
+            requestAudioPermissions()
         }
 
         binding.permissionAppearOnTopCard.setOnClickListener {
-            if (!Settings.canDrawOverlays(this@PermissionActivity)) {
-                val intent = Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:${this@PermissionActivity.packageName}")
-                )
-                this@PermissionActivity.startActivityForResult(intent, OVERLAY_REQUEST_CODE)
-            }
+            requestOverlayPermission()
 
-            binding.startMainActivity.isEnabled = Settings.canDrawOverlays(this@PermissionActivity)
-
-//            overlayPermissions.send {
-//                binding.startMainActivity.isEnabled = allPermissions.checkStatus().allGranted()
-//            }
         }
 
+        overlayPermissionLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                binding.startMainActivity.isEnabled = isAllPermissionsAreGranted()
+            } else {
+                Toast.makeText(this, "Overlay permission not granted", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.startMainActivity.isEnabled = isAllPermissionsAreGranted()
+    }
+
+
+    private fun isAllPermissionsAreGranted(): Boolean {
+        return Settings.canDrawOverlays(this) &&
+                (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) ==
+                PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestOverlayPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:${this.packageName}")
+            )
+            overlayPermissionLauncher.launch(intent)
+        }
+    }
+
+    private fun requestAudioPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+                Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG).show()
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), RECORD_AUDIO_REQUEST_CODE)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), RECORD_AUDIO_REQUEST_CODE)
+            }
+        } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            binding.startMainActivity.isEnabled = isAllPermissionsAreGranted()
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -64,15 +102,19 @@ class PermissionActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == OVERLAY_REQUEST_CODE) {
-            if (Settings.canDrawOverlays(this) && allPermissions.checkStatus().allGranted()) {
-                binding.startMainActivity.isEnabled = true
+        when (requestCode) {
+            RECORD_AUDIO_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    binding.startMainActivity.isEnabled = isAllPermissionsAreGranted()
+                } else {
+                    Toast.makeText(this, "Permissions Denied to record audio", Toast.LENGTH_LONG).show()
+                }
+                return
             }
         }
     }
 
     companion object {
-        const val OVERLAY_REQUEST_CODE = 1
+        const val RECORD_AUDIO_REQUEST_CODE = 1
     }
-
 }
